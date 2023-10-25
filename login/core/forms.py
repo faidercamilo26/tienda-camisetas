3a78d45f9b580aceeb540eda44123bcff3b33cca
faidercamilo <olayafaider@gmail.com>
2023-10-25 14:42:31 -0500

    union de proyectos

--- login/core/forms.py
+++ login/core/forms.py
@@ -3,7 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    Direccion = forms.CharField(max_length=255, required=True, help_text='Ingresa tu dirección de residencia.')
+    ID_Usuario = forms.IntegerField(max_value=9999999999, required=True, help_text='Ingresa tu número de identificación')
+    document_choices = [
+        ('CC', 'Cédula de Ciudadanía'),
+        ('TI', 'Tarjeta de Identidad'),
+        ('CE', 'Cédula de Extranjería'),
+        
+    ]
+    Tipo_Documento = forms.ChoiceField(choices=document_choices, required=True, help_text='Selecciona tu tipo de documento')
     
+    telefono = forms.IntegerField(max_value=9999999999, required=True, help_text='Ingresa tu número de telefono')
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'Tipo_Documento', 'ID_Usuario', 'email', 'telefono', 'Direccion', 'password1', 'password2']
--- login/login/settings.py
+++ login/login/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
